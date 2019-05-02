842ab2a103681e6077bd643b44cce0ea9bce132e
Victor Mendele <victor.mendele68@gmail.com>
2019-05-02 15:41:41 +0200

    test: remove integration and unused

--- tests/Functional/CitiesEndpointsTest.php
+++ tests/Functional/CitiesEndpointsTest.php
@@ -6,21 +6,21 @@ use Slim\Http\Response;
 
 class CitiesEndpointsTest extends BaseTestCase
 {
-    public function testGetIndex ()
-    {
-        /**
-         * @var Response
-         */
-        $response = $this->runApp('GET', '/cities');
+    // public function testGetIndex ()
+    // {
+    //     /**
+    //      * @var Response
+    //      */
+    //     $response = $this->runApp('GET', '/cities');
         
-        $this->assertEquals(
-            $response->getStatusCode(),
-            200
-        );
+    //     $this->assertEquals(
+    //         $response->getStatusCode(),
+    //         200
+    //     );
         
-        $this->assertEquals(
-            strpos($response->getHeader('Content-Type')[0], 'application/json') !== false,
-            true
-        );
-    }
+    //     $this->assertEquals(
+    //         strpos($response->getHeader('Content-Type')[0], 'application/json') !== false,
+    //         true
+    //     );
+    // }
 }

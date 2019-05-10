b2bf00590c53cd0b9c685a25b931be4493f7d0f9
Victor Mendele <victor.mendele68@gmail.com>
2019-05-10 16:06:02 +0200

    :hammer:fix: bdd connection charset

--- src/settings.php
+++ src/settings.php
@@ -23,7 +23,7 @@ return [
             'host' => getenv('DB_HOST'),
             'username' => getenv('DB_USERNAME'),
             'password' => getenv('DB_PASSWORD'),
-            'charset' => 'utf8',
+            // 'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix' => '',
             'args' => (getenv('ENV') === 'DEV') ? [

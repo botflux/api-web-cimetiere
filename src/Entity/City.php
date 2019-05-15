a993b7cf38b90eaba65347e939a270ba99134ead
Victor Mendele <victor.mendele68@gmail.com>
2019-05-15 11:56:42 +0200

    refactor: put request code of CityController to CityModel

--- src/Entity/City.php
+++ src/Entity/City.php
@@ -28,4 +28,41 @@ class City extends Model
         'adresse',
         'id'
     ];
+
+    /**
+     * Find all cities
+     *
+     * @param array $wheres
+     * @param array $order
+     * @param int $page
+     * @param int $pageSize
+     * @return array
+     */
+    public static function find (array $wheres, array $order, $page, $pageSize) {
+        $citiesRequest = City::where($wheres);
+
+         if (\is_array($order) && isset($order['by']) && isset($order['direction'])) {
+            $citiesRequest = $citiesRequest
+                ->orderBy ($order['by'], $order['direction'])
+            ;
+        }
+
+        return $citiesRequest
+            ->take($pageSize)
+            ->skip($page * $pageSize)
+            ->get()
+        ;
+    }
+
+    /**
+     * Returns the count
+     *
+     * @param array $wheres
+     * @return int
+     */
+    public static function count (array $wheres) {
+        return City::where ($wheres)
+            ->count()
+        ;
+    }
 }

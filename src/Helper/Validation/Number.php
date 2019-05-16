bb9ca9ffd59c5f85ebc308a53f4930539ade7db2
Victor Mendele <victor.mendele68@gmail.com>
2019-05-16 15:54:27 +0200

    :hammer:fix: number validation

--- src/Helper/Validation/Number.php
+++ src/Helper/Validation/Number.php
@@ -6,26 +6,66 @@ class Number {
 
     private $interval = [];
 
-    public function __construct ($interval) {
+    public function __construct ($interval = []) {
         $this->interval = $interval;
+
+        if (isset($interval['from']) && isset($interval['to'])) {
+            if ($interval['from'] >= $interval['to']) {
+                throw new \Exception ("The _from_ parameter can't be greater or equal to _to_");
+            }
+        }
     }
 
     public function __invoke ($v) {
-        $v = $v ?? 0;
+        $value = $v ?? 0;
 
-        if (\ctype_digit ($v)) {
-            $v = intval ($v);
+        /**
+         * If this is a string
+         */
+        if (\is_string($value)) {
+            /** If this is a float */
+            if (\intval($value) != floatval($value)) {
+                return false;
+            }
+
+            /** We parse only integer if $v is a string */
+            $value = intval($value);
         }
 
-        if (!is_int($v)) {
-            return false;
-        }
+        /** If $v is a float */
+        if (!is_int($value)) return false;
 
+        /** If an interval was specified */
         if (count($this->interval) > 0) {
-
             return $v >= $this->interval['from'] && $v <= $this->interval['to'];
         }
 
         return true;
+        // $v = $v ?? 0;
+        // \var_dump ($v);
+
+        // if (\is_string($v)) {
+        //     if (!\is_numeric ($v)) return false;
+
+        //     if (\intval($v) != \floatval($v)) return false;
+        // }
+
+        // // if (\is_numeric ($v)) {
+        // //     $v = intval ($v);
+
+        // //     if ($v != \floatval($v))
+        // //         return false;
+        // // }
+
+        // if (!is_int($v)) {
+        //     return false;
+        // }
+
+        // if (count($this->interval) > 0) {
+
+        //     return $v >= $this->interval['from'] && $v <= $this->interval['to'];
+        // }
+
+        // return true;
     }
 }
